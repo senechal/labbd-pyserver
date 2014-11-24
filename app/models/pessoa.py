689b5337597e6a1617c669a6e9d88bd0c1a62dc9
André Luiz Lé Sénéchal Paolino <andre.senechal@hotmail.com>
2014-11-24 00:46:06 -0200

    Alteração dos modelos que tem sequencia, o metodo sobreescrevido foi refeito de uma maneira mais logica. O retorno de todas os contorles de criação e edição agoram retornam o objeto criado/editado, alem da mensagem de sucesso

--- app/models/pessoa.py
+++ app/models/pessoa.py
@@ -7,26 +7,6 @@ class Pessoa(BaseModel):
 	def create(self):
 		self.idPe = self.getSequenceId()
 		super(Pessoa, self).create()
-		# fields = self.__class__.fields 
-		# last = fields[-1]
-		# values = ''
-		# tpl = ()
-		# i =1
-		# for item in fields:
-		# 	value = self.__dict__[item]
-		# 	tpl = tpl + (value,)
-		# 	if item == last:
-		# 			values = values +' :'+str(i)
-		# 	else:
-		# 		values = values +' :'+str(i)+', '
-		# 	i = i+1
-		# sql = 'INSERT INTO ' + self.__class__.__name__.lower() + ' VALUES ( ' + values + ' )'
-		# print sql
-		# db.connect()
-		# transaction = db.transaction(sql,tpl)
-		# db.disconnect()
-		# if transaction.get('error') == 1:
-		# 	raise ValueError(transaction) 
 
 	def getSequenceId(self):
 		db.connect()
